d273e3932083387f468abf08d0ec300b75653979
tobi187 <fischerstobi1998@gmail.com>
2022-03-04 19:50:52 +0100

    Add project files.

---
+++ PermutationWPF/Logic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationWPF
+{
+    internal class Logic
+    {
+        Logic(int ebenen, int tabellen, int zahlen)
+        {
+            AnzahlEbenen = ebenen;
+            AnzahlTabellen = tabellen;
+            AnzahlZahlen = zahlen;
+        }
+
+        public int AnzahlEbenen;
+        public int AnzahlTabellen;
+        public int AnzahlZahlen;
+        
+        public void Step(string ebene)
+        {
+            string[] numbers = ebene.Split(" ");
+        }
+
+
+    }
+}
